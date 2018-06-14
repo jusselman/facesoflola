d641c0bdab5a14f58b08cec6670ee259f16ffa03
jusselman <usselmanjosh@gmail.com>
2018-06-13 21:23:21 -0700

    facesoflola

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// function applyAtt(target, att, attVal) {
+//     document.getElementById(target).setAttribute(att,attVal);
+// }
+//
+// function removeAtt(target,att) {
+//     document.getElementById(target).removeAttribute(att);
+// }
